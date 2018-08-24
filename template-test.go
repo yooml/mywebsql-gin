7e190f0f1cc332a1ff244d40ff92ec92399e420f
余梦磊 <yumenglei@souche.com>
2018-08-24 10:11:31 +0800

    test vue

--- template-test.go
+++ template-test.go
@@ -1,9 +1,10 @@
 package main
 import (
 	"github.com/gin-gonic/gin"
-	//"html/template"
+	"html/template"
 	"net/http"
 	"mywebsql-gin/controllers"
+	"github.com/gin-gonic/gin/render"
 )
 
 type StructA struct {
@@ -11,13 +12,18 @@ type StructA struct {
 }
 
 
+type MyHTMLRender struct {
+	templates map[string]*template.Template
+}
+
 func main() {
 	router := gin.Default()
-	router.LoadHTMLGlob("views/*")
+	//router.LoadHTMLGlob("views/*")
 //router.LoadHTMLGlob("views/*.html")
 //html := template.Must(template.ParseFiles("views/mywebsql-base.html","views/sysuser.html"))
 //html := template.Must(template.ParseGlob("views/*"))
 //router.SetHTMLTemplate(html)
+	router.HTMLRender = createMyRender()
 	db,_ := controllers.InitDB()
 	defer db.Close()
 	router.Static("/static", "./static")
@@ -32,8 +38,35 @@ func main() {
 	router.POST("/sysuserdb",controllers.Sysuserdb)
 	router.GET("/sysuserdb",controllers.Sysuserdb)
 
-//router.GET("/test",IndexApi)
+	router.GET("/test",func(c *gin.Context) {
+		c.HTML(http.StatusOK, "views/testvue.html",nil)
+	})
+	router.GET("/test2",func(c *gin.Context) {
+		c.HTML(http.StatusOK, "test3",nil)
+	})
 
 	router.Run(":8080")
 }
 
+
+func (r *MyHTMLRender) Add(name string, html *template.Template) {
+	if r.templates == nil {
+		r.templates = make(map[string]*template.Template)
+	}
+	r.templates[name] = html
+}
+
+func (r *MyHTMLRender) Instance(name string, data interface{}) render.Render {
+	return render.HTML{
+		Template: r.templates[name],
+		//Data:     data,
+	}
+}
+
+func createMyRender() render.HTMLRender {
+	r := &MyHTMLRender{}
+	r.Add("test3", template.Must(template.ParseFiles("views/testvue2.html")))
+
+
+	return r
+}
